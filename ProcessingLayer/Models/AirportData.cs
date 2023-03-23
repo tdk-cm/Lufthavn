fca3f7e7c2102aea0060ee168d42a75f9a767b47
tdk-cm <cme@td-k.dk>
2023-03-23 08:55:17 +0100

    added processingabstraction

---
+++ ProcessingLayer/Models/AirportData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lufthavn_Final.ProcessingLayer.Models
+{
+    internal class AirportData
+    {
+        public string FullName { get; set; }
+        public string IataCode { get; set; }
+    }
+}
---
+++ ProcessingLayer/Models/OperatorData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lufthavn_Final.ProcessingLayer.Models
+{
+    internal class OperatorData
+    {
+        public int CompanyId { get; set; }
+        public string CompanyName { get; set; }
+    }
+}
